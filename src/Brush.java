f513a2911460b1da5f62ca153aa0e20d1edd5941
Daniel Commins <dencee@users.noreply.github.com>
2022-02-24 16:28:33 -0800

    Added instructions

--- src/Brush.java
+++ src/Brush.java
@@ -9,58 +9,23 @@ public class Brush {
     int brushSize, maxBrushThickness, numberOfBristles, brushStagger;
     Bristle[] bristles;
 
-    class Bristle {
-        int startX, startY, endX, endY;
-        int radius, startRadius;
-
-        Bristle(int x, int y, int r) {
-            startX = x;
-            startY = y;
-            endX = x;
-            endY = y;
-            radius = r;
-            startRadius = r;
-        }
-
-        void update() {
-            if (endX == (x + startX) && endY == (y + startY)) {
-                radius += 1;
-            } else {
-                radius = startRadius;
-            }
-
-            endX = x + startX;
-            endY = y + startY;
-        }
-
-        void draw() {
-            int pixel = painting.get(endX, endY);
-            processing.stroke(pixel);
-
-            if (endX == (x + startX) && endY == (y + startY)) {
-                processing.strokeWeight(radius / 50);
-                processing.point(endX, endY);
-            } else {
-                float dist = PApplet.dist(x + startX, y + startY, endX, endY);
-
-                // The bigger the distance, i.e. the faster the mouse is moved,
-                // the bigger and more coarse the line width
-                processing.strokeWeight((radius * dist) / 50);
-                processing.line(x + startX, y + startY, endX, endY);
-            }
-        }
-    }
-
-    public Brush(PApplet processing, PImage painting) {
-        this.painting = painting;
+    /*
+     * Brush settings
+     */
+    static int DEFAULT_MAX_BRUSH_THICKNESS = 20;
+    static int DEFAULT_NUM_BRISTLES = 200;
+    static int DEFAULT_BRUSH_SIZE = 40;
+    static int DEFAULT_BRUSH_STAGGER = 6;
+    
+    public Brush(PApplet processing) {
         this.processing = processing;
         this.paintingIndex = 0;
         this.x = 0;
         this.y = 0;
-        this.brushSize = 40;
-        this.brushStagger = 6;
-        this.maxBrushThickness = 20;
-        this.numberOfBristles = 200;
+        this.brushSize = DEFAULT_BRUSH_SIZE;
+        this.brushStagger = DEFAULT_BRUSH_STAGGER;
+        this.maxBrushThickness = DEFAULT_MAX_BRUSH_THICKNESS;
+        this.numberOfBristles = DEFAULT_NUM_BRISTLES;
         this.bristles = new Bristle[this.numberOfBristles];
 
         initializeBristles();
@@ -112,5 +77,47 @@ public class Brush {
 
         // Sets window to move to the upper left corner
         processing.getSurface().setLocation(10, 10);
+    }
+    
+    class Bristle {
+        int startX, startY, endX, endY;
+        int radius, startRadius;
+
+        Bristle(int x, int y, int r) {
+            startX = x;
+            startY = y;
+            endX = x;
+            endY = y;
+            radius = r;
+            startRadius = r;
+        }
+
+        void update() {
+            if (endX == (x + startX) && endY == (y + startY)) {
+                radius += 1;
+            } else {
+                radius = startRadius;
+            }
+
+            endX = x + startX;
+            endY = y + startY;
+        }
+
+        void draw() {
+            int pixel = painting.get(endX, endY);
+            processing.stroke(pixel);
+
+            if (endX == (x + startX) && endY == (y + startY)) {
+                processing.strokeWeight(radius / 50);
+                processing.point(endX, endY);
+            } else {
+                float dist = PApplet.dist(x + startX, y + startY, endX, endY);
+
+                // The bigger the distance, i.e. the faster the mouse is moved,
+                // the bigger and more coarse the line width
+                processing.strokeWeight((radius * dist) / 50);
+                processing.line(x + startX, y + startY, endX, endY);
+            }
+        }
     }
 }
--- src/VanGogh.java
+++ src/VanGogh.java
@@ -1,15 +1,67 @@
 import processing.core.PApplet;
 import processing.core.PImage;
 
+/*
+ * Goal: Create a program to paint some Van Gogh paintings!
+ * 
+ * In this class:
+ * 1. Declare a PImage array variable as a member variable in this class.
+ *    DO NOT initialize it.
+ * 
+ * 2. In the initializePaintings() method, initialize the PImage array to
+ *    contain 4 images.
+ * 
+ * 3. Use the loadImage() method to assign each of the 4 images in the /images
+ *    folder to an index in the PImage array
+ *    "starryNight.jpg", "strawHatPortrait.jpg",
+ *    "wheatField.jpg", "painterOnRoad.jpg"
+ * 
+ * 4. Declare a member variable to keep track of the current image index from the
+ *    array. This variable will indicate which image to display from the array.
+ * 
+ * 5. In the initializePaintings() method, initialize the variable to 0
+ *    so the first painting to select is the first one in the array.
+ * 
+ * 6. Call the setNewPainting() method from the brush object and pass in
+ *    the current painting as an input variable. It should be similar to this:
+ *    brush.setNewPainting(paintings[currentIndex]);
+ * 
+ * 7. Run the program. You should be able to paint with your brush when
+ *    pressing the mouse and dragging. The paint will be thicker the faster
+ *    the mouse moves and more detailed when moving the mouse slowly.
+ *    Press 'r' to remove all the paint from the canvas and start over.
+ * 
+ * 8. In the selectNextPainting() method, increase the image index variable
+ *    by 1. Add code to make sure the index variable is never larger than the
+ *    size of the painting array and wraps back to the first painting.
+ * 
+ * 9. Call the setNewPainting() method from the brush object similar to step 6.
+ * 
+ * 10.Run the program. Press the spacebar to move to the next painting.
+ *    Make sure that pressing the spacebar on the fourth painting returns
+ *    back to the first one.
+ * 
+ * 11.EXTRA: Find more paintings and add them to your code.
+ *           Watercolor paintings work the best!
+ */
+
 public class VanGogh extends PApplet {
-    PImage[] paintings;
-    PImage starryNight, portrait, wheatField, painterOnRoad;
-    PImage currentPainting;
     PImage canvas;
     PImage paintbrushCursor;
-    Brush brush;
-    int currentPaintingIndex;
     boolean initializeCanvas = true;
+    
+    /*
+     * Write your code below
+     */
+    Brush brush;
+    
+    void initializePaintings() {
+        
+    }
+    
+    void selectNextPainting() {
+        
+    }
 
     @Override
     public void settings() {
@@ -20,29 +72,17 @@ public class VanGogh extends PApplet {
 
     @Override
     public void setup() {
-        canvas = loadImage("canvas.jpg");
-        starryNight = loadImage("starryNight.jpg");
-        portrait = loadImage("strawHatPortrait.jpg");
-        wheatField = loadImage("wheatField.jpg");
-        painterOnRoad = loadImage("painterOnRoad.jpg");
-
-        paintings = new PImage[4];
-        paintings[0] = starryNight;
-        paintings[1] = portrait;
-        paintings[2] = wheatField;
-        paintings[3] = painterOnRoad;
-        currentPaintingIndex = 0;
-        currentPainting = paintings[currentPaintingIndex];
-
         surface.setResizable(true);
-        surface.setSize(currentPainting.width, currentPainting.height);
-        surface.setLocation(10, 10);
+        
+        canvas = loadImage("canvas.jpg");
+        
+        brush = new Brush(this);
 
         paintbrushCursor = loadImage("paintbrushCur.png");
         paintbrushCursor.resize(22 * 2, 28 * 2);
         cursor(paintbrushCursor);
 
-        brush = new Brush(this, currentPainting);
+        initializePaintings();
     }
 
     @Override
@@ -64,12 +104,9 @@ public class VanGogh extends PApplet {
                 image(canvas, 0, 0);
             } else if (key == 32) {
                 // SPACE key pressed--go to next painting
-                currentPaintingIndex = (currentPaintingIndex + 1) % paintings.length;
-                currentPainting = paintings[currentPaintingIndex];
+                selectNextPainting();
 
-                brush.setNewPainting(currentPainting);
-
-                canvas.resize(width, height);
+                //canvas.resize(width, height);
                 initializeCanvas = true;
             }
         }
